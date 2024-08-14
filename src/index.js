a00bba540bf46329a154a0015004d0307b483fc8
mavlekiev <mavlekiev@gmail.com>
2024-08-15 00:26:21 +0400

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -38,9 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  // write your solution here
+  let str = '';
+  let strKey = '';
+  
+ 
+  for (let i = 0; i < expr.length; i++) {
+
+    if (expr[i] === "1" && expr[i + 1] === "0") {
+        strKey += ".";
+        i += 1;
+      } else if (expr[i] === "1" && expr[i + 1] === "1") {
+        strKey += "-";
+        i += 1;
+      }
+    if ((i+1) % 10 === 0) {
+        str += MORSE_TABLE[strKey]; 
+        strKey = '';
+    }
+    if (expr[i] === "*") {
+        str += " ";
+        i += 9;
+      }
+   
+
+    /*
+    while (expr[i] !== "*" || i < expr.length) {
+      if (expr[i] === "1" && expr[i + 1] === "0") {
+        strKey += ".";
+      } else if (expr[i] === "1" && expr[i + 1] === "1") {
+        strKey += "-";
+      }
+    }
+    str += MORSE_TABLE[strKey]; 
+    if (expr[i] === "*") {
+        str += " ";
+        i += 9;
+      }
+    */
+  }
+  return str;
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
